741666f561d8238c2fdfdcbb4d63c81b485d2e25
GaelicGrime <will.angus.blaylock@gmail.com>
2022-05-23 19:33:22 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# SPREADINATOR
+
+  - A personal utility to randomly spread source files into a number of destination directories where I will step through them and keep the good, ditch the bad.
+  - I also use it to even up some daily data to be archived.
+
+
+
+
+######
---
+++ setup.py
@@ -0,0 +1,36 @@
+
+
+#
+#
+# * #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
+# * start of CF.setup.py
+# * #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
+#
+#
+
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+  author="GaelicGrime",
+  author_email="will.angus.blaylock@gmail.com",
+  license="GPLv3",
+  name="Bulk File Manager",
+  url="https://github.com/ComfortableSoftware/commonFunctions_py",
+  version="0.1.0",
+  package_dir={"bulkFileManager": "bulkFileManager"},
+  package_data={
+      "bulkFileManager": [
+          "../doc/*",
+      ]
+  },
+  packages=["bulkFileManagerk"],
+  install_requires=[
+      "CF",
+  ],
+  extras_require={
+      ],
+  }
+)
